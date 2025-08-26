21ae87767b26a2e477dc3a033b5b6325f664320e
Albert <brozynaalbert@gmail.com>
2025-08-26 02:29:02 +0200

    push

--- lib/features/auth/register/pages/register_page.dart
+++ lib/features/auth/register/pages/register_page.dart
@@ -17,17 +17,19 @@ class _RegisterPageState extends State<RegisterPage> {
   final TextEditingController _firstNameController = TextEditingController();
   final TextEditingController _lastNameController = TextEditingController();
 
-  void handleLoginButton(BuildContext context){
-    Navigator.push(context,MaterialPageRoute(builder: (context) =>LoginPage()));
+  void handleLoginButton(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => LoginPage()),
+    );
   }
 
   // Method to check password complexity
-  bool checkPasswordComplexity(){
+  bool checkPasswordComplexity() {
     // TODO check password complexity
 
     return true;
   }
-
 
   void handleRegister() {
     final firstName = _firstNameController.text;
@@ -39,15 +41,19 @@ class _RegisterPageState extends State<RegisterPage> {
     createUserWithEmailAndPassword();
   }
 
-  Future<void> createUserWithEmailAndPassword() async{
-    try{
-      final userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
-          email: _emailController.text.trim().toLowerCase(),
-          password: _passwordController.text.trim()
-      );
+  Future<void> createUserWithEmailAndPassword() async {
+    try {
+      final userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+            email: _emailController.text.trim().toLowerCase(),
+            password: _passwordController.text.trim(),
+          );
       // TODO IN FINAL TO DELETE
       print(userCredential);
-    }on FirebaseAuthException catch(e){
+
+      // TODO create a new user
+
+    } on FirebaseAuthException catch (e) {
       print("Auth error ${e.message}");
     }
   }
@@ -55,83 +61,101 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(16),
-        child: Column(
-          children: [
-            // First Name
-            TextField(
-              // Bounding with controller
-              controller: _firstNameController,
-              // What keyboard to show
-              keyboardType: TextInputType.text,
-              // Decoration of the input
-              decoration: InputDecoration(
-                labelText: "First Name",
-                prefixIcon: Icon(Icons.person),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(12)),
+      appBar: AppBar(title: Text("Log in")),
+      backgroundColor: Colors.transparent,
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/background-start.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            children: [
+              // First Name
+              TextField(
+                // Bounding with controller
+                controller: _firstNameController,
+                // What keyboard to show
+                keyboardType: TextInputType.text,
+                // Decoration of the input
+                decoration: InputDecoration(
+                  labelText: "First Name",
+                  prefixIcon: Icon(Icons.person),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(12)),
+                  ),
                 ),
               ),
-            ),
-            // Last name
-            TextField(
-              // Bounding with controller
-              controller: _lastNameController,
-              // What keyboard to show
-              keyboardType: TextInputType.text,
-              // Decoration of the input
-              decoration: InputDecoration(
-                labelText: "last name",
-                prefixIcon: Icon(Icons.person),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(12)),
+              // Last name
+              TextField(
+                // Bounding with controller
+                controller: _lastNameController,
+                // What keyboard to show
+                keyboardType: TextInputType.text,
+                // Decoration of the input
+                decoration: InputDecoration(
+                  labelText: "last name",
+                  prefixIcon: Icon(Icons.person),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(12)),
+                  ),
                 ),
               ),
-            ),
-            // Email field
-            TextField(
-              // Bounding with controller
-              controller: _emailController,
-              // What keyboard to show
-              keyboardType: TextInputType.emailAddress,
-              // Decoration of the input
-              decoration: InputDecoration(
-                labelText: "Email",
-                prefixIcon: Icon(Icons.email),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(12)),
+              // Email field
+              TextField(
+                // Bounding with controller
+                controller: _emailController,
+                // What keyboard to show
+                keyboardType: TextInputType.emailAddress,
+                // Decoration of the input
+                decoration: InputDecoration(
+                  labelText: "Email",
+                  prefixIcon: Icon(Icons.email),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(12)),
+                  ),
                 ),
               ),
-            ),
-            // Break between inputs
-            SizedBox(height: 16),
-            TextField(
-              controller: _passwordController,
-              keyboardType: TextInputType.visiblePassword,
-              decoration: InputDecoration(
-                labelText: "Password",
-                prefixIcon: Icon(Icons.password),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(12)),
+              // Break between inputs
+              SizedBox(height: 16),
+              TextField(
+                controller: _passwordController,
+                keyboardType: TextInputType.visiblePassword,
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  prefixIcon: Icon(Icons.password),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(12)),
+                  ),
                 ),
               ),
-            ),
-            TextField(
-              controller: _repeatPasswordController,
-              keyboardType: TextInputType.visiblePassword,
-              decoration: InputDecoration(
-                labelText: "Password",
-                prefixIcon: Icon(Icons.password),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(12)),
+              TextField(
+                controller: _repeatPasswordController,
+                keyboardType: TextInputType.visiblePassword,
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  prefixIcon: Icon(Icons.password),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(12)),
+                  ),
                 ),
               ),
-            ),
-            // Login
-            ElevatedButton(onPressed: handleRegister, child: Text("Register")),
-            TextButton(onPressed: () => handleLoginButton(context), child: Text("Log in"))
-          ],
+              // Login
+              ElevatedButton(
+                onPressed: handleRegister,
+                child: Text("Register"),
+              ),
+              TextButton(
+                onPressed: () => handleLoginButton(context),
+                child: Text("Log in"),
+              ),
+            ],
+          ),
         ),
       ),
     );
